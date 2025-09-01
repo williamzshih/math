8cb989f63d38923642ca2fbe46847f1eba0f02a5
williamzshih <148172235+williamzshih@users.noreply.github.com>
2025-09-01 01:49:44 -0700

    create slider size setting

--- src/app/math/page.tsx
+++ src/app/math/page.tsx
@@ -2,16 +2,34 @@
 
 import { OrbitControls } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
+import { useState } from "react";
+import { Card, CardHeader, CardTitle, CardContent } from "@/components/ui/card";
+import { Slider } from "@/components/ui/slider";
 
 export default function Math() {
+  const [size, setSize] = useState(0.5);
+
   return (
     <div className="h-screen w-screen">
+      <Card className="absolute top-4 left-4 z-10 w-sm">
+        <CardHeader>
+          <CardTitle>Settings</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <Slider
+            value={[size]}
+            onValueChange={(size) => setSize(size[0])}
+            max={1}
+            step={0.01}
+          />
+        </CardContent>
+      </Card>
       <Canvas>
         <ambientLight intensity={0.1} />
         <directionalLight color="red" position={[0, 0, 5]} />
         <axesHelper args={[5]} />
         <mesh>
-          <boxGeometry args={[2, 2, 2]} />
+          <boxGeometry args={[size, size, size]} />
           <meshStandardMaterial />
         </mesh>
         <OrbitControls />
