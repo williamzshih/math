8a6f609b995520146162991d9ae3930132b7a10b
williamzshih <148172235+williamzshih@users.noreply.github.com>
2025-08-31 23:45:40 -0700

    configure lint and prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,7 @@
+import { FlatCompat } from "@eslint/eslintrc";
+import perfectionist from "eslint-plugin-perfectionist";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -8,6 +9,15 @@ const __dirname = dirname(__filename);
 const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
+
+perfectionist.configs["recommended-natural"].rules = {
+  "perfectionist/sort-imports": [
+    "error",
+    {
+      newlinesBetween: 0,
+    },
+  ],
+};
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
@@ -20,6 +30,7 @@ const eslintConfig = [
       "next-env.d.ts",
     ],
   },
+  perfectionist.configs["recommended-natural"],
 ];
 
 export default eslintConfig;
