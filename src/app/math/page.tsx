1b6d171ae4ac2a0ba537dfd95a2b3a6cf8b2795f
williamzshih <148172235+williamzshih@users.noreply.github.com>
2025-09-01 00:38:35 -0700

    create simple scene

--- src/app/math/page.tsx
+++ src/app/math/page.tsx
@@ -1,3 +1,21 @@
+"use client";
+
+import { OrbitControls } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
+
 export default function Math() {
-  return <div>Math</div>;
+  return (
+    <div className="h-screen w-screen">
+      <Canvas>
+        <ambientLight intensity={0.1} />
+        <directionalLight color="red" position={[0, 0, 5]} />
+        <axesHelper args={[5]} />
+        <mesh>
+          <boxGeometry args={[2, 2, 2]} />
+          <meshStandardMaterial />
+        </mesh>
+        <OrbitControls />
+      </Canvas>
+    </div>
+  );
 }
