41eb97e3ca5082e68aa11dd045641bf92d598d02
williamzshih <148172235+williamzshih@users.noreply.github.com>
2025-09-03 18:23:39 +0000

    update devcontainer installations

--- create.sh
+++ create.sh
@@ -2,5 +2,19 @@
 
 set -euo pipefail
 
+echo "💬 Installing Bun"
 curl -fsSL https://bun.sh/install | bash
 source /home/node/.bashrc
+
+echo "💬 Installing emscripten"
+cd ~
+git clone https://github.com/emscripten-core/emsdk.git
+cd emsdk
+./emsdk install latest
+./emsdk activate latest
+source ./emsdk_env.sh
+
+echo "💬 Installing clangd"
+sudo apt update
+sudo apt-get install clangd-19
+sudo update-alternatives --install /usr/bin/clangd clangd /usr/bin/clangd-19 100
